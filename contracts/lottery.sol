6b1ecdcf34fb1ad54b7e690dc944ce370b528fa7
David Simek <david.simek1998@gmail.com>
2021-09-09 21:50:32 +0200

    initial commit

---
+++ contracts/lottery.sol
@@ -0,0 +1,61 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+
+contract Lottery {
+    address public manager;
+    address payable[] public players;
+
+    event pickedWinner(address indexed winner);
+
+    constructor(){
+        manager = msg.sender;
+    }
+
+    receive() external payable {
+      require(
+        msg.value == 0.1 ether,
+        'require to sent exactly 0.1 ether'
+      );
+      players.push(payable(msg.sender));
+    }
+
+    function getBalance() public view returns(uint){
+      require(
+        msg.sender == manager,
+        'Only manager can retrieve balance of contract'
+      );
+      return address(this).balance;
+    }
+
+    function random() public view returns(uint){
+      // Not the most approriate way how to get random number
+      // Using oracle is much safer
+      uint randomNum = uint(keccak256(
+        abi.encodePacked(
+          block.difficulty, block.timestamp, players.length
+        )
+      ));
+      return randomNum;
+    }
+
+    function pickWinner() public {
+      require(
+        msg.sender == manager,
+        'Only manager of the lottery can pick the winner'
+      );
+      require(
+        players.length >= 3,
+        'Minimum 3 players are required to pick winner'
+      );
+      uint r = random();
+      address payable winner;
+      uint index = r % players.length;
+      winner = players[index];
+      winner.transfer(getBalance());
+      players = new address payable[](0); // reseting the lottery
+      emit pickedWinner(winner);
+    }
+}
---
+++ test/lottery-test.js
@@ -0,0 +1,100 @@
+const { expect } = require("chai");
+
+describe("Lottery", function () {
+  let Lottery, lottery, manager, addr1, addr2, addr3, addr4;
+
+  beforeEach(async () => {
+    Lottery = await ethers.getContractFactory('Lottery')
+    lottery = await Lottery.deploy();
+    [manager, addr1, addr2, addr3, addr4] = await ethers.getSigners();
+  });
+
+
+  describe('Deployment', () => {
+    it("Should set manager to deployers address", async () => {
+      expect(await lottery.manager()).to.equal(manager.address);
+    });
+  });
+
+  describe("Contract methods", ()=> {
+
+    it("Should allowed to entry players only if they pay exactly 0.1 ether", async() =>{
+      await expect(addr1.sendTransaction({
+          to: lottery.address,
+          value: ethers.utils.parseEther("0.05")
+      })).to.be.revertedWith('require to sent exactly 0.1 ether');
+
+      await expect(addr1.sendTransaction({
+          to: lottery.address,
+          value: ethers.utils.parseEther("1.00")
+      })).to.be.revertedWith('require to sent exactly 0.1 ether');
+
+      await addr1.sendTransaction({
+          to: lottery.address,
+          value: ethers.utils.parseEther("0.1")
+      });
+      expect(await lottery.players(0)).to.be.equal(addr1.address);
+    });
+
+    it("Only manager should be able to get balance of contract", async() =>{
+      await expect(
+        lottery.connect(addr1).getBalance()
+      ).to.be.revertedWith(
+        'Only manager can retrieve balance of contract'
+      );
+
+      await addr1.sendTransaction({
+          to: lottery.address,
+          value: ethers.utils.parseEther("0.1")
+      });
+
+      expect(
+        await lottery.getBalance()
+      ).to.be.equal(
+        ethers.utils.parseEther("0.1")
+      );
+
+    });
+
+    describe("Pick random winner", async() =>{
+      it("Only manager should be able to call pick winner method", async() =>{
+        await expect(
+          lottery.connect(addr1).pickWinner()
+        ).to.be.revertedWith(
+          'Only manager of the lottery can pick the winner'
+        );
+      });
+
+      it("Minimum players should be 3", async() =>{
+        await expect(
+          lottery.pickWinner()
+        ).to.be.revertedWith(
+          'Minimum 3 players are required to pick winner'
+        );
+      });
+
+      it("Transfer lottery funds to the winner address and reset players index", async() =>{
+        let players = [addr1, addr2, addr3];
+        await addr1.sendTransaction({
+            to: lottery.address,
+            value: ethers.utils.parseEther("0.1")
+        });
+        await addr2.sendTransaction({
+            to: lottery.address,
+            value: ethers.utils.parseEther("0.1")
+        });
+        await addr3.sendTransaction({
+            to: lottery.address,
+            value: ethers.utils.parseEther("0.1")
+        });
+        await expect(
+          lottery.pickWinner()
+        ).to.emit(
+          lottery, 'pickedWinner'
+        );
+      });
+
+    });
+
+  });
+});
